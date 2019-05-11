615014cdc32529562d1ad3eda45d4a6fbd7e1c1c
slavizdravkov <slavizdravkov@gmail.com>
2019-05-11 15:09:20 +0300

    Resolve assignment8

---
+++ assignment-8/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <h2>Amazing Content</h2>
+                </component>
+                <!--<app-blue>
+                    <h3>Blue</h3>
+                </app-blue>
+                <app-green>
+                    <h4>Green</h4>
+                </app-green>
+                <app-red>
+                    <h2>Red</h2>
+                </app-red>-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data () {
+            return {
+                selectedComponent: 'appBlue'
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import "~bootstrap/scss/bootstrap";
+</style>
